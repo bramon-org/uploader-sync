014c0edc66f7ba81c11834c90aeeff0187f013a8
Thiago Paes <mrprompt@gmail.com>
2020-09-19 01:48:23 -0300

    Fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,6 @@ from uploader.scripts.cli import cli
 
 def test_cli_count():
     runner = CliRunner()
-    result = runner.invoke(cli, ['./', '*P.jpg', 'output.jpg'])
+    result = runner.invoke(cli, ['./'])
     assert result.exit_code == 0
     assert result.output == "- Reading captures\n- Nothing to do\n"
