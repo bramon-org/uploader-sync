811dae7f8d656055e080a751794ddcd9456f0f14
Thiago Paes <mrprompt@gmail.com>
2020-09-19 01:46:34 -0300

    Remove local json and get stations from API

--- uploader/scripts/cli.py
+++ uploader/scripts/cli.py
@@ -1,7 +1,6 @@
 import click
 import glob
 import requests
-import json
 import re
 from uploader import data
 
@@ -10,20 +9,25 @@ def get_matching_captures(captures_dir: list):
     return glob.glob("{}/**/*".format(captures_dir), recursive=True)
 
 
+def get_stations():
+    headers = {'Authorization': "Bearer {}".format(data.API_TOKEN)}
+    stations = requests.get(data.API_URL.format("v1/admin/stations?limit=1000"), headers=headers)
+
+    return stations.json()
+
+
 def get_station_id(station_name: str):
-    from uploader import data
+    stations_list = get_stations()
+    stations_data = stations_list['data']
 
-    with open(data.STATIONS_MAP) as data_file:
-        data = json.load(data_file)
-
-    return list(filter(lambda x: x["name"] == station_name, data))
+    return list(filter(lambda x: x["name"] == station_name, stations_data))
 
 
 def get_station_name(capture):
     base = re.findall('(\w{3,5})_(\d{1,})(.+)?\.(\w{3})$', capture)
 
     if len(base) == 0:
-        return False
+        raise IndexError("Station not found")
 
     station = base[0][2].replace('_', '')
 
@@ -38,20 +42,21 @@ def get_station_name(capture):
 
 def upload_captures(captures: list):
     for capture in captures:
-        station_name = get_station_name(capture)
-
-        if not station_name:
+        try:
+            station_name = get_station_name(capture)
+        except IndexError:
             continue
 
         station = get_station_id(station_name).pop()
         station_id = station['id']
         user_id = station['user_id']
+
         files = {'files[]': open(capture, 'rb')}
         values = {'station_id': station_id, 'user_id': user_id}
         headers = {'Authorization': "Bearer {}".format(data.API_TOKEN)}
+        upload = requests.post(data.API_URL.format("v1/admin/captures"), files=files, data=values, headers=headers)
 
-        r = requests.post(data.API_URL, files=files, data=values, headers=headers)
-        click.echo(r.text)
+        click.echo(upload.text)
 
 
 @click.command('uploader')
